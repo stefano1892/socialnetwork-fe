5e04ceb9521667b0130e5cc9a1a417bace221cf3
Stefano Calcaterra <37615901+stefano1892@users.noreply.github.com>
2023-01-07 17:39:25 +0100

    style e componenti

--- src/components/addNewPost/addNewPost.tsx
+++ src/components/addNewPost/addNewPost.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { Card } from '@mui/material';
 
 import "./style.scss"
+import { Button, Form } from 'react-bootstrap';
 
 const AddNewPost = () => {
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log("hai inserito un nuovo post")
+  }
 
   return (
     <>
       <Card className='cardContainer'>
-        Aggiungi nuovo post
+        <Form onSubmit={(e) => handleSubmit(e)}>
+          <Form.Group controlId='insertNewPost'>
+            <Form.Control as='textarea' className='textarea' rows={3} placeholder='Inserisci un nuovo post' />
+          </Form.Group>
+          <Button variant="primary" className='submitButton' type="submit">
+            Submit
+          </Button>
+        </Form>
       </Card>
     </>
   )
