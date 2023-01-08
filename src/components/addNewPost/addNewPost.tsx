a11e60b3d4ea197ff59c14d257d326a4454cd6e2
Stefano Calcaterra <37615901+stefano1892@users.noreply.github.com>
2023-01-08 15:37:01 +0100

    installazione fontawesome e crazione bottoni per carica foto e video

--- src/components/addNewPost/addNewPost.tsx
+++ src/components/addNewPost/addNewPost.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import { Card } from '@mui/material';
+import { Card, Chip } from '@mui/material';
+import { Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileVideo, faImage } from '@fortawesome/free-regular-svg-icons';
+import iconPotoSvg from './img/iconPhoto.png';
 
 import "./style.scss"
-import { Button, Form } from 'react-bootstrap';
+import { Stack } from '@mui/system';
 
 const AddNewPost = () => {
 
   const handleSubmit = (e: any) => {
-    e.preventDefault()
-    console.log("hai inserito un nuovo post")
+    console.log(e)
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault();
+      console.log('hai inserito un post')
+    }
   }
 
   return (
     <>
       <Card className='cardContainer'>
-        <Form onSubmit={(e) => handleSubmit(e)}>
+        <Form>
           <Form.Group controlId='insertNewPost'>
-            <Form.Control as='textarea' className='textarea' rows={3} placeholder='Inserisci un nuovo post' />
+            <Form.Control
+              as='textarea'
+              className='textarea'
+              rows={3}
+              placeholder='Inserisci un nuovo post'
+              onKeyDown={(e) => handleSubmit(e)}
+            />
           </Form.Group>
-          <Button variant="primary" className='submitButton' type="submit">
-            Submit
-          </Button>
+          <Stack direction="row" className='chipContainer' spacing={1}>
+            <div className='photoChip'>
+              <img src={iconPotoSvg} className='photoIcon' /> Foto
+            </div>
+            <div className='photoChip'>
+              <img src={iconPotoSvg} className='photoIcon' /> Video
+            </div>
+          </Stack>
         </Form>
       </Card>
     </>
