2384df8ba8179a1343c025db5152813fcfd8d9b4
Stefano Calcaterra <37615901+stefano1892@users.noreply.github.com>
2023-01-12 17:11:45 +0100

    prima bozza colonna sinistra homepage

--- src/components/home/leftColumn/leftColumn.tsx
+++ src/components/home/leftColumn/leftColumn.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ImageProfile from '../img/foto_profilo.jpg'
 import './style.scss'
+import icona_gruppi from './img/icona_gruppi.png'
+import icona_eventi from './img/icona_eventi.png'
+import icona_impostazioni from './img/icona_impostazioni.png'
+import icona_utenti from './img/icona_utenti.png'
 
 const HomeLeftColumn = () => {
+
+  const goToProfile = () => {
+    console.log("vai al profilo")
+  }
 
   return (
     <>
@@ -21,7 +29,21 @@ const HomeLeftColumn = () => {
         </div>
         <div className='separator' />
       </div>
-      <div className='cardFooter'>
+      <div className='container'>
+        <div className='menuItem'>
+          <img src={icona_eventi} alt='event_icon' className='menuIcon' /> Eventi
+        </div>
+        <div className='menuItem'>
+          <img src={icona_gruppi} alt='groups_icon' className='menuIcon' /> Gruppi
+        </div>
+        <div className='menuItem'>
+          <img src={icona_utenti} alt='users_icon' className='menuIcon' /> Utenti
+        </div>
+        <div className='menuItem'>
+          <img src={icona_impostazioni} alt='settings_icon' className='menuIcon' /> Impostazioni
+        </div>
+      </div>
+      <div className='cardFooter' onClick={() => goToProfile()}>
         Vedi profilo
       </div>
     </>
